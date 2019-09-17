d71dff7a3b50d8d70ae41eac17dd8ffb78a1ba72
LucasZGoransson <46808945+LucasZGoransson@users.noreply.github.com>
2019-09-17 09:49:11 +0200

    Delete productsnav.php

--- php/productsnav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-	<ul>
-		<li><a href="index.php">Start</a></li>
-		<li><a href="products.php">Produkter</a></li>
-		<li><a href="sida3.php">Varusida</a></li>
-		<li><a href="login.php">Logga in</a></li>
-	</ul>
-</nav>
