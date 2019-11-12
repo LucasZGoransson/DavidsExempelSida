00bc412617aea4d0c6721c482036afece8d37e6b
LucasZGoransson <46808945+LucasZGoransson@users.noreply.github.com>
2019-11-12 10:49:53 +0100

    Lista
    Lagt till lista och användare

--- dbcon.mysqli.php
+++ dbcon.mysqli.php
@@ -8,21 +8,114 @@
 		exit;
 	}
 	
-	$sql="SELECT * FROM produkter";
 	
+	$sql="SELECT * FROM kunder";
 	$result = $db->query($sql);
-	
+	echo "<table>";
 	while($row=$result->fetch_assoc())
 	{
-		echo $row['namn'];
-		echo ", ";
-		echo $row['beskrivning'];
-		echo ", ";
-		echo $row['pris'];
-		echo ", ";
-		echo $row['bild'];
-		echo "<br>";
+		
+		echo "<tr><td>";
+		echo $row['anvnamn'];
+		echo "</td>";
+				
+		echo "<td>";
+		echo $row['fornamn'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['efternamn'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['adress'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['postnummer'];
+		echo "</td>";
+
+		echo "<td>";
+		echo $row['postadress'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['telefon'];
+		echo "</td></tr>";
 	}
+	echo "</table>";
+	
+	
+	$sql="SELECT * FROM orders";
+	$result = $db->query($sql);
+	echo "<table>";
+	while($row=$result->fetch_assoc())
+	{
+		echo "<tr><td>";
+		echo $row['orderID'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['produktID'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['antal'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['kundID'];
+		echo "</td></tr>";
+	}
+	echo "</table>";
+	
+	
+	$sql="SELECT * FROM produkter";
+	$result = $db->query($sql);
+	echo "<table>";
+		while($row=$result->fetch_assoc())
+	{
+		echo "<tr><td>";
+		echo $row['namn'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['beskrivning'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['pris'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['bild'];
+		echo "</td></tr>";
+	}
+	echo "</table>";
+	
+	
+	$sql="SELECT * FROM users";
+	$result = $db->query($sql);
+	echo "<table>";
+		while($row=$result->fetch_assoc())
+	{
+		echo "<tr><td>";
+		echo $row['anvnamn'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['email'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['password'];
+		echo "</td>";
+		
+		echo "<td>";
+		echo $row['status'];
+		echo "</td></tr>";
+	}
+	echo "</table>";
 	
 	
 	mysqli_close($db);
--- webbshop.sql
+++ webbshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 22 okt 2019 kl 10:33
+-- Tid vid skapande: 12 nov 2019 kl 10:47
 -- Serverversion: 10.4.6-MariaDB
 -- PHP-version: 7.3.8
 
@@ -25,33 +25,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `anvandare`
---
-
-CREATE TABLE `anvandare` (
-  `username` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
-  `email` varchar(150) COLLATE utf8_swedish_ci NOT NULL,
-  `losenord` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
-  `status` tinyint(2) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
-
---
--- Dumpning av Data i tabell `anvandare`
---
-
-INSERT INTO `anvandare` (`username`, `email`, `losenord`, `status`) VALUES
-('kalle123', 'kalle@exempel.se', 'qwe123', 1),
-('pelle', 'pelle@exempel.se', 'qwe123', 1);
-
--- --------------------------------------------------------
-
---
 -- Tabellstruktur `kunder`
 --
 
 CREATE TABLE `kunder` (
   `kundID` int(10) UNSIGNED NOT NULL,
-  `användarnamn` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
+  `anvnamn` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
   `fornamn` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
   `efternamn` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
   `adress` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
@@ -64,8 +43,10 @@ CREATE TABLE `kunder` (
 -- Dumpning av Data i tabell `kunder`
 --
 
-INSERT INTO `kunder` (`kundID`, `användarnamn`, `fornamn`, `efternamn`, `adress`, `postnummer`, `postadress`, `telefon`) VALUES
-(1, 'kalle123', 'kalle', 'svensson', 'exempel1', 12345, 'VAXJO', '1234567890');
+INSERT INTO `kunder` (`kundID`, `anvnamn`, `fornamn`, `efternamn`, `adress`, `postnummer`, `postadress`, `telefon`) VALUES
+(1, 'Kalle1', 'Kalle1', 'Svensson1', 'Abovagen1', 12345, 'Vaxjo', '1234567890'),
+(2, 'Kalle2', 'Kalle2', 'Svensson2', 'AbovAgen2', 12345, 'Vaxjo', '1234567890'),
+(3, 'Kalle3', 'Kalle3', 'Svensson3', 'Abovagen3', 12345, 'Vaxjo', '1234567890');
 
 -- --------------------------------------------------------
 
@@ -85,7 +66,9 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`orderID`, `produktID`, `antal`, `kundID`) VALUES
-(1, 1, 2, 1);
+(1, 1, 1, 1),
+(2, 2, 2, 2),
+(3, 3, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -106,27 +89,43 @@ CREATE TABLE `produkter` (
 --
 
 INSERT INTO `produkter` (`produktID`, `namn`, `beskrivning`, `pris`, `bild`) VALUES
-(1, 'äpple', 'en frukt', 10, 'image/apple.jpg');
+(1, 'Banan', 'Gul', 100, '123456'),
+(2, 'Apple', 'Rod', 200, '654321'),
+(3, 'Avocado', 'Svart', 50, '321456'),
+(4, 'Paron', 'Morkgron', 200, '123654');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE `users` (
+  `anvnamn` varchar(50) COLLATE utf8_swedish_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8_swedish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_swedish_ci NOT NULL,
+  `status` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumpning av Data i tabell `users`
+--
+
+INSERT INTO `users` (`anvnamn`, `email`, `password`, `status`) VALUES
+('Kalle1', 'Kalle1@hotmail.com', 'qwe123', 1),
+('Kalle2', 'Kalle2@hotmail.com', 'qwe123', 1),
+('Kalle3', 'Kalle3@hotmail.com', 'qwe123', 1);
 
 --
 -- Index för dumpade tabeller
 --
 
 --
--- Index för tabell `anvandare`
---
-ALTER TABLE `anvandare`
-  ADD PRIMARY KEY (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD KEY `losenord` (`losenord`);
-
---
 -- Index för tabell `kunder`
 --
 ALTER TABLE `kunder`
   ADD PRIMARY KEY (`kundID`),
-  ADD KEY `användarnamn` (`användarnamn`);
+  ADD KEY `anvnamn` (`anvnamn`);
 
 --
 -- Index för tabell `orders`
@@ -143,6 +142,15 @@ ALTER TABLE `produkter`
   ADD PRIMARY KEY (`produktID`);
 
 --
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `anvnamn` (`anvnamn`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `anvnamn_2` (`anvnamn`),
+  ADD UNIQUE KEY `anvnamn_3` (`anvnamn`);
+
+--
 -- AUTO_INCREMENT för dumpade tabeller
 --
 
@@ -150,19 +158,19 @@ ALTER TABLE `produkter`
 -- AUTO_INCREMENT för tabell `kunder`
 --
 ALTER TABLE `kunder`
-  MODIFY `kundID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `kundID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT för tabell `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `orderID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `orderID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT för tabell `produkter`
 --
 ALTER TABLE `produkter`
-  MODIFY `produktID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `produktID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restriktioner för dumpade tabeller
@@ -172,19 +180,14 @@ ALTER TABLE `produkter`
 -- Restriktioner för tabell `kunder`
 --
 ALTER TABLE `kunder`
-  ADD CONSTRAINT `kunder_ibfk_1` FOREIGN KEY (`användarnamn`) REFERENCES `anvandare` (`username`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `kunder_ibfk_1` FOREIGN KEY (`anvnamn`) REFERENCES `users` (`anvnamn`) ON UPDATE CASCADE;
 
 --
 -- Restriktioner för tabell `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`kundID`) REFERENCES `kunder` (`kundID`) ON UPDATE CASCADE;
-
---
--- Restriktioner för tabell `produkter`
---
-ALTER TABLE `produkter`
-  ADD CONSTRAINT `produkter_ibfk_1` FOREIGN KEY (`produktID`) REFERENCES `orders` (`produktID`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`produktID`) REFERENCES `produkter` (`produktID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`kundID`) REFERENCES `kunder` (`kundID`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
